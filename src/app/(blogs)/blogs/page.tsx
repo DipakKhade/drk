68ee12f1a3a2afa51756d009fdfe61d5ff906307
= <=>
2024-07-26 23:37:47 +0530

    rm vim script

--- src/app/(blogs)/blogs/page.tsx
+++ src/app/(blogs)/blogs/page.tsx
@@ -112,45 +112,45 @@ const Page = () => {
           {
             // visible  &&  visible.length>0 &&
             <ul className="flex flex-wrap sm:grid- w-full gap-4 md:max-w-screen-xl md:px-6 min-h-screen">
-            {posts?.map((post: Post, index: number) => (
-              <div
-                key={index}
-                className="group relative flex flex-col space-y-2 w-[75vw] sm:w-[30vw]"
-              >
-                <div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-sm h-[400px]">
-                  <div className="flex-1">
-                    <Image
-                      className="rounded-sm object-cover h-[200px] w-full"
-                      src={
-                        "http:" + post?.fields?.coverImage?.fields?.file?.url
-                      }
-                      height={200}
-                      width={500}
-                      alt={post?.fields?.coverImage?.fields?.title}
-                    />
-                  </div>
-                  <div className="p-6 flex-1 overflow-hidden">
-                    <h5 className="block truncate mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-                      {post?.fields?.title}
-                    </h5>
-                    <p className="block truncate font-sans text-base antialiased font-light leading-relaxed text-inherit">
-                      {post?.fields?.exceprt}
-                    </p>
-                  </div>
-                  <div className="p-6 pt-0">
-                    <Link href={`/blogs/${post?.fields?.slug}`}>
-                      <button
-                        className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
-                        type="button"
-                      >
-                        Read More
-                      </button>
-                    </Link>
+              {posts?.map((post: Post, index: number) => (
+                <div
+                  key={index}
+                  className="group relative flex flex-col space-y-2 w-[75vw] sm:w-[30vw]"
+                >
+                  <div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-sm h-[400px]">
+                    <div className="flex-1">
+                      <Image
+                        className="rounded-sm object-cover h-[200px] w-full"
+                        src={
+                          "http:" + post?.fields?.coverImage?.fields?.file?.url
+                        }
+                        height={200}
+                        width={500}
+                        alt={post?.fields?.coverImage?.fields?.title}
+                      />
+                    </div>
+                    <div className="p-6 flex-1 overflow-hidden">
+                      <h5 className="block truncate mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+                        {post?.fields?.title}
+                      </h5>
+                      <p className="block truncate font-sans text-base antialiased font-light leading-relaxed text-inherit">
+                        {post?.fields?.exceprt}
+                      </p>
+                    </div>
+                    <div className="p-6 pt-0">
+                      <Link href={`/blogs/${post?.fields?.slug}`}>
+                        <button
+                          className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+                          type="button"
+                        >
+                          Read More
+                        </button>
+                      </Link>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </ul>
+              ))}
+            </ul>
           }
         </div>
       </main>
